b94d7d9351212e2881ab71539ff24d6f714834a4
victor manzanero loeza <victor.manzanero@globant.com>
2022-11-03 17:27:34 -0600

    this commit is for new dashboard

---
+++ UIKitApp-main/UIKitApp/MainDash/Controller/WelcomeTwoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  UIKitApp
+//
+//  Created by victor manzanero on 03/11/22.
+//
+
+import UIKit
+
+class WelcomeTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closed(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
